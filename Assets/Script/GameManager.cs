105c6b3352cb4ddab4b5410ddbbf19960646d2fc
Eduardo Lozano Munera <eduardo.lozano@hutchgames.com>
2017-06-15 16:46:38 +0100

    Avances (3 finales) y rework de mostrar imagenes

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+
+	public List<SpriteRenderer> BGImages = new List<SpriteRenderer>();
+
+
+	public void ChangeImage(string ImageName)
+	{
+		SpriteRenderer r = BGImages.FirstOrDefault(x => x.name == ImageName);
+		if (r == null)
+		{
+			Debug.LogError("Error: Not found image " + ImageName);
+			return;
+		}
+
+		foreach (SpriteRenderer s in BGImages)
+		{
+			s.enabled = (s.name == r.name);
+		}
+	}
+
+}
